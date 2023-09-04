811e0b0084a5effa027cccfed39ba84b26bbfcdd
khjgggg <guswjd7@gmail.com>
2023-09-04 21:33:59 +0900

    [NEW] : 🆕(feature/data): 더미데이터, drawable리소스,font, 컬러추가, .gitignore

---
+++ app/src/main/java/com/example/coding10/BabyDummy.kt
@@ -0,0 +1,28 @@
+package com.example.coding10
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class BabyDummy(val aIcon: Int,
+                     val aIcon2: Int,
+                     val aName: String,
+                     val aAge: String,
+                     val aNumber: String,
+                     val aEmail: String,
+                     val aBloodType: String,
+                     val aMemo: String,
+                     ) : Parcelable
+
+val dataList = mutableListOf<BabyDummy>().apply {
+    add(BabyDummy(R.drawable.baby1,R.drawable.baby1_2,"올리비아","13개월","010-1621-1422","asjk@naver.com","A형","ㄷ겨쇼ㅜㅈ디ㅑㄷ어"))
+    add(BabyDummy(R.drawable.baby2,R.drawable.baby2_2,"지우","3살","010-1423-1460","mmk@naver.com","A형","코로나로 힘겨운 요즘인데 평온한 하루 보내시길 바랍니다."))
+    add(BabyDummy(R.drawable.baby3,R.drawable.baby3_2,"서연","4살","010-8647-8953","wld@naver.com","B형","사계절의 즐거움으로 가득한 하루 되세요. :)"))
+    add(BabyDummy(R.drawable.baby4,R.drawable.baby4_2,"하윤","2살","010-1531-4620","qkf@naver.com","B형","행복한 시간들이 함께하길 바래요!"))
+    add(BabyDummy(R.drawable.baby5,R.drawable.baby5_2,"민서","3살","010-7485-9462","werd@naver.com","AB형","이웃님! 오늘도 어김없이 찾아와 주셔서 감사합니다."))
+    add(BabyDummy(R.drawable.baby6,R.drawable.baby6_2,"벤틀리","3살","010-8615-4286","zlsk@naver.com","AB형","나는 벤틀리! 이렇게 만나뵙게 되어 너무 기뻐요!"))
+    add(BabyDummy(R.drawable.baby7,R.drawable.baby7_2,"윌리엄","6살","010-1682-9782","jki@naver.com","O형","아녕하세요. 윌리엄이에오. 엉아는 울지 않아여!"))
+    add(BabyDummy(R.drawable.baby8,R.drawable.baby8_2,"엠마","2살","010-7681-1827","kkj@naver.com","O형","니ㅏㅓㄹ@#\$ㅓㅁㄴㅇ랴ㅕ587"))
+    add(BabyDummy(R.drawable.baby9,R.drawable.baby9_2,"이사벨라","5살","010-4715-8562","ggik@naver.com","AB형","방문해주셔서 고마워요~~"))
+    add(BabyDummy(R.drawable.baby10,R.drawable.baby10_2,"이한","130일","010-9846-6847","jiaj@naver.com","O형","다 읽으셨다면 좋아요 꾹~!"))
+}
