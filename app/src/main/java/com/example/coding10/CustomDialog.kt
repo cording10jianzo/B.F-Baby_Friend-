c28e84564edac8ffa1144bf5c44cbe9dd0387547
yspark2 <rotqjfrkffo@gmail.com>
2023-09-09 02:13:43 +0900

    [FEAT] : ⚙️: 회원가입 기능 보완 - textwatcher

--- app/src/main/java/com/example/coding10/CustomDialog.kt
+++ app/src/main/java/com/example/coding10/CustomDialog.kt
@@ -39,7 +39,6 @@ class CustomDialog(
 ) : DialogFragment() {
     //    private val dialog = Dialog(context)
     private lateinit var binding: DialogLayoutBinding
-    private var hashMap = HashMap<String, String>()
 
     //notification 알람
     private val Notification: Notification by lazy {
@@ -52,22 +51,7 @@ class CustomDialog(
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = DialogLayoutBinding.inflate(inflater, container, false)
-
-        // 추가할 데이터 변수
-        var dialog_name = binding.dialogName
-        var dialog_number = binding.dialogNumber
-        var dialog_email = binding.dialogEmail
-        var dialog_memo = binding.dialogMemo
-        var dialog_age = binding.dialogAge
-        var dialog_bloodtype = binding.dialogBloodtype
-
-        var dialog_name_error = binding.dialogNameError
-        var dialog_number_error = binding.dialogPhoneError
-        var dialog_email_error = binding.dialogEmailError
-        var dialog_age_error = binding.dialogAgeError
-        var dialog_bloodtype_error = binding.dialogBloodError
-
+        binding = DialogLayoutBinding.inflate(inflater, container, false)
 
         //Event 시간에 맞춰 Notification 표시
         binding.dialogNotification1.setOnClickListener {
@@ -101,7 +85,7 @@ class CustomDialog(
                     notificationManager.notify(notificationId, notificationBuilder.build())
                 }
 
-                Toast.makeText(context, "5초 후에 알림이 표시됩니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "5분 후에 알림이 표시됩니다.", Toast.LENGTH_SHORT).show()
             }, delayMillis)
 
         }
@@ -135,7 +119,7 @@ class CustomDialog(
                     notificationManager.notify(notificationId, notificationBuilder.build())
                 }
 
-                Toast.makeText(context, "5초 후에 알림이 표시됩니다.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(context, "10분 후에 알림이 표시됩니다.", Toast.LENGTH_SHORT).show()
             }, delayMillis)
         }
 
@@ -145,41 +129,31 @@ class CustomDialog(
             dismiss()
         }
 
+        // 추가할 데이터 변수
+        var dialogName = binding.dialogName
+        var dialogNumber = binding.dialogNumber
+        var dialogEmail = binding.dialogEmail
+        var dialogMemo = binding.dialogMemo
+        var dialogAge = binding.dialogAge
+        var dialogBloodType = binding.dialogBloodType
+
+        var dialogNameError = binding.dialogNameError
+        var dialogNumberError = binding.dialogNumberError
+        var dialogEmailError = binding.dialogEmailError
+        var dialogAgeError = binding.dialogAgeError
+        var dialogBloodtypeError = binding.dialogBloodError
+
+        with(binding) {// 순서 - 이름, 번호, 메일, 나이, 피
+            dialogName.addTextChangedListener(createNameTextWatcher(dialogName))
+            dialogNumber.addTextChangedListener(createNumberTextWatcher(dialogNumber))
+            dialogEmail.addTextChangedListener(createMailTextWatcher(dialogEmail))
+            dialogAge.addTextChangedListener(createAgeTextWatcher(dialogAge))
+            dialogBloodType.addTextChangedListener(createBloodTypeTextWatcher(dialogBloodType))
+        }
         // Save 버튼
         binding.dialogSave.setOnClickListener {
 
-            if (dialog_name_error.visibility == View.INVISIBLE &&
-                dialog_number_error.visibility == View.INVISIBLE &&
-                dialog_email_error.visibility == View.INVISIBLE &&
-                dialog_age_error.visibility == View.INVISIBLE &&
-                dialog_bloodtype_error.visibility == View.INVISIBLE
-            ) {
-                init(2)
-                infoProcess("edit")
-                Toast.makeText(requireContext(), "회원가입 완료!", Toast.LENGTH_SHORT).show()
-                // 입력한 데이터 MainItems에 추가하고 onSave를 통해 데이터 뿌려주기
-                val contact = MainItems(
-                    getUri(R.drawable.baby1), getUri(R.drawable.baby1_2),
-                    aName = dialog_name.toString(), aNumber = dialog_number.toString(),
-                    aEmail = dialog_email.toString(), aMemo = dialog_memo.toString(),
-                    //처음 다이얼로그에서 입력한 Edit는 4개였지만 MainItems(더미데이터)의 데이터는
-                    //aName,aNumber,aEmail,aMemo,aAge,aBloodType 총 6개 이므로 아래 2개를 추가
-                    aAge = dialog_age.toString(), aBloodType = dialog_bloodtype.toString()
-                )
-                onSave(contact)
-                dismiss()
 
-            } else {
-                Snackbar.make(binding.root, "입력 값을 확인해주세요.", Snackbar.LENGTH_SHORT).show()
-            }
-
-//            val dialog_name = dialog_name.text
-//            val dialog_number = dialog_number.text
-//            val dialog_email = dialog_email.text
-//            val dialog_memo = dialog_memo.text
-//            val dialog_age = dialog_age.text
-//            val dialog_bloodtype = dialog_bloodtype.text
-//
 //                if (dialog_name.toString().trim().isEmpty()) {
 //                    Toast.makeText(requireContext(), "이름을 입력하세요.", Toast.LENGTH_SHORT).show()
 //                } else if (!Pattern.matches("^[가-힣]*\$", dialog_name)) {
@@ -220,26 +194,69 @@ class CustomDialog(
 //                onSave(contact)
 //                dismiss()
 //            }
-        }
 
+            // 조건 추가하기 ( not null + @ )
+            if (dialogNameError.visibility == View.INVISIBLE &&
+                dialogNumberError.visibility == View.INVISIBLE &&
+                dialogEmailError.visibility == View.INVISIBLE &&
+                dialogAgeError.visibility == View.INVISIBLE &&
+                dialogBloodtypeError.visibility == View.INVISIBLE &&
+                dialogName.text.toString().trim().isNotEmpty() &&
+                dialogNumber.text.toString().trim().isNotEmpty() &&
+                dialogEmail.text.toString().trim().isNotEmpty() &&
+                dialogAge.text.toString().trim().isNotEmpty() &&
+                dialogBloodType.text.toString().trim().isNotEmpty()
+            ) {
+                Toast.makeText(requireContext(), "회원가입 완료!", Toast.LENGTH_SHORT).show()
+
+//                myPageEtBlood.text = Editable.Factory.getInstance().newEditable(hashMap["혈액형"])
+//                hashMap["혈액형"] = myPageEtBlood.text.toString()
+//
+//                binding.dialogName
+//                Editable.Factory.getInstance().newEditable(hashMap["혈액형"])
+
+                // 입력한 데이터 MainItems에 추가하고 onSave를 통해 데이터 뿌려주기
+                val contact = MainItems (
+                    getUri(R.drawable.baby1), getUri(R.drawable.baby1_2),
+                    aName = binding.dialogName.text.toString(), aNumber = binding.dialogNumber.text.toString(),
+                    aEmail = binding.dialogEmail.text.toString(), aMemo = binding.dialogMemo.text.toString(),
+                    //처음 다이얼로그에서 입력한 Edit는 4개였지만 MainItems(더미데이터)의 데이터는
+                    //aName,aNumber,aEmail,aMemo,aAge,aBloodType 총 6개 이므로 아래 2개를 추가
+                    aAge = binding.dialogAge.text.toString(), aBloodType = binding.dialogBloodType.text.toString()
+
+                )
+                onSave(contact)
+                dismiss()
+
+            } else {
+                Snackbar.make(binding.root, "입력 값을 확인해주세요.", Snackbar.LENGTH_SHORT).show()
+            }
+
+        }
         return binding.root
     }
 
-    private fun scheduleNotification(delayMillis: Long, message: String) {
-        // 지연 후 알림 생성 및 표시
-        val notificationBuilder = Notification.createNotification("알림이 예약되었습니다", message)
-        Notification.showNotification(1, notificationBuilder)
-    }
-
+    //dialog_name
     private fun createNameTextWatcher(editText: EditText): TextWatcher {
         return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) =
                 with(binding) {
                     val id = s.toString()
-                    val regexPattern1 = Regex("[가-힣]")
-
-                    if (regexPattern1.matches(id) && id.isNotEmpty()) {
+                    val regexPattern = Regex("\\b[가-힣]{2,}\\b")
+                    if (id.isNotEmpty() && regexPattern.matches(id)) {
                         dialogNameError.visibility = View.INVISIBLE
                     } else {
                         dialogNameError.visibility = View.VISIBLE
@@ -251,16 +268,71 @@ class CustomDialog(
         }
     }
 
-    private fun createEmailTextWatcher(editText: EditText): TextWatcher {
+    //dialog_number
+    private fun createNumberTextWatcher(editText: EditText): TextWatcher {
         return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) =
                 with(binding) {
+                    val regexPatternLength = Regex("^.{1,13}\$")
                     val id = s.toString()
-                    val regexPattern1 = Regex("^[a-zA-Z0-9]+@[a-zA-Z0-9]+\$")
+                    val regexPattern1 = Regex("^\\d{3}-\\d{4}-\\d{4}$")
+                    val regexPattern2 = Regex("^[A-Za-z]")
+                    if (regexPattern1.matches(id) && id.isNotEmpty() && regexPatternLength.matches(
+                            id
+                        )
+                    ) {
+                        dialogNumberError.visibility = View.INVISIBLE
+                    } else if (regexPatternLength.matches(id)) {
+                        dialogNumberError.visibility = View.INVISIBLE
+                    } else if (regexPattern2.matches(id)) {
+                        dialogNumberError.visibility = View.VISIBLE
+                    } else {
+                        dialogNumberError.visibility = View.VISIBLE
+                    }
+                }
 
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+    }
 
-                    if (regexPattern1.matches(id) && id.isNotEmpty()) {
+    //dialog_email
+    private fun createMailTextWatcher(editText: EditText): TextWatcher {
+        return object : TextWatcher {
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) =
+                with(binding) {
+
+                    val id = s.toString()
+                    val emailRegexPattern = Regex("^[A-Za-z0-9+_.-]{1,12}+@(.+)\$")
+                    val regexPatternLength = Regex("^.{1,13}\$")
+
+                    if (id.isNotEmpty() && emailRegexPattern.matches(id)) {
                         dialogEmailError.visibility = View.INVISIBLE
                     } else {
                         dialogEmailError.visibility = View.VISIBLE
@@ -272,38 +344,36 @@ class CustomDialog(
         }
     }
 
-    private fun createphonTextWatcher(editText: EditText): TextWatcher {
-        return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
-                with(binding) {
-                    val id = s.toString()
-                    val regexPattern1 = Regex("^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$")
-
-                    if (regexPattern1.matches(id) && id.isNotEmpty()) {
-                        dialogPhoneError.visibility = View.INVISIBLE
-                    } else {
-                        dialogPhoneError.visibility = View.VISIBLE
-                    }
-                }
-
-            override fun afterTextChanged(s: Editable?) {
-            }
-        }
-    }
-
+    //dialog_age
     private fun createAgeTextWatcher(editText: EditText): TextWatcher {
         return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
-                with(binding) {
-                    val id = s.toString()
-                    val regexPattern1 = Regex("\"^[0-7]\$\"")
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
 
-                    if (regexPattern1.matches(id) && id.isNotEmpty()) {
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) =
+                with(binding) {
+
+                    val id = s.toString()
+                    val regexPattern = Regex("[1-7]")
+
+                    if (id.isNotEmpty() && regexPattern.matches(id)) {
+
                         dialogAgeError.visibility = View.INVISIBLE
+
                     } else {
+
                         dialogAgeError.visibility = View.VISIBLE
+
                     }
                 }
 
@@ -312,15 +382,33 @@ class CustomDialog(
         }
     }
 
+    //dialog_blood_type
     private fun createBloodTypeTextWatcher(editText: EditText): TextWatcher {
         return object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) =
-                with(binding) {
-                    val id = s.toString()
-                    val regexPattern1 = Regex("^[A-Za-z]")
+            override fun beforeTextChanged(
+                s: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+            }
 
-                    if (regexPattern1.matches(id) && id.isNotEmpty()) {
+            override fun onTextChanged(
+                s: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) =
+                with(binding) {
+
+                    val id = s.toString()
+                    val regexPatternLength = Regex("^.{1,2}\$")
+                    val inputText = Regex("[ABOabo]+")
+
+                    if (id.isNotEmpty() && inputText.matches(id) && regexPatternLength.matches(
+                            id
+                        )
+                    ) {
                         dialogBloodError.visibility = View.INVISIBLE
                     } else {
                         dialogBloodError.visibility = View.VISIBLE
@@ -332,66 +420,12 @@ class CustomDialog(
         }
     }
 
-    private fun init(switch: Int) = with(binding) {
-
-        when (switch) {
-            1 -> {
-
-                dialogName.isEnabled = true
-                dialogAge.isEnabled = true
-                dialogEmail.isEnabled = true
-                dialogBloodtype.isEnabled = true
-                dialogNumber.isEnabled = true
-
-            }
-
-            2 -> {
-                dialogName.isEnabled = false
-                dialogAge.isEnabled = false
-                dialogEmail.isEnabled = false
-                dialogBloodtype.isEnabled = false
-                dialogNumber.isEnabled = false
-
-            }
-
-            else -> {
-
-            }
-        }
+    private fun scheduleNotification(delayMillis: Long, message: String) {
+        // 지연 후 알림 생성 및 표시
+        val notificationBuilder = Notification.createNotification("알림이 예약되었습니다", message)
+        Notification.showNotification(1, notificationBuilder)
     }
 
-    private fun infoProcess(data: String) = with(binding) {
-
-        when (data) {
-            "edit" -> {
-                hashMap["이름"] = dialogName.text.toString()
-                hashMap["혈액형"] = dialogBloodtype.text.toString()
-                hashMap["번호"] = dialogNumber.text.toString()
-                hashMap["이메일"] = dialogEmail.text.toString()
-                hashMap["나이"] = dialogAge.text.toString()
-
-//                hashMap["혈액형"]?.let { Log.d("test", it) }
-            }
-
-            "cancel" -> {
-
-                dialogName.text = Editable.Factory.getInstance().newEditable(hashMap["이름"])
-                dialogNumber.text = Editable.Factory.getInstance().newEditable(hashMap["번호"])
-                dialogBloodtype.text = Editable.Factory.getInstance().newEditable(hashMap["혈액형"])
-                dialogEmail.text = Editable.Factory.getInstance().newEditable(hashMap["이메일"])
-                dialogAge.text = Editable.Factory.getInstance().newEditable(hashMap["나이"])
-
-            }
-
-            else -> {
-
-            }
-
-
-
-        }
-
-    }
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "My Channel"
